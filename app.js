55b0134f22fbe66eedcb35820af6fcc4dae5cc27
avazbek <avazbek@gmail.com>
2025-06-07 12:02:28 +0500

    ..

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
+// Array1. n natural soni berilgan. Dastlabki n ta toq sondan tashkil topgan massiv qaytaruvchi getInitialOdds(n) nomli funksiya tuzing.
+// Input: getInitialOdds(5)
+// Output: [1, 3, 5, 7, 9]
+
+// function getInitialOdds(n) {
+//     let result = [];
+//     for (let i = 0; i < n; i++) {
+//         result.push(i*2+1);
+//     }
+//     return result;
+// }
+// console.log(getInitialOdds(5));
+
+
 // Array2. arr nomli massiv berilgan. Massiv elementlari orasidan juftlarini indekslari kamayish tartibidan hosil bo’lgan massivni qaytaruvchi getEvenReverse(arr) programma tuzilsin.
 // Input: [4, 5, 7, 8, 6, 9]
 // Output: [6, 8, 4]
 
-// const arr = [4, 5, 7, 8, 6, 9];
-// const newArr = [];
-// for (let i = 0; i < arr.length; i++) {
-//     if(arr[i] % 2 == 0) {
-//         newArr.unshift(arr[i]);
-//     }
-// }
-// console.log(newArr);
+// let arr = [4, 5, 7, 8, 6, 9];
+// let newarr = [];
 
+// function getEvenReverse(arr) {
+//     for (let i = arr.length; i >= 0; i--) {
+//         if(arr[i] % 2 == 0) {
+//             newarr.push(arr[i]);
+//         }
+//     }
+//     return newarr;
+// }
+// console.log(getEvenReverse(arr));
 
 
 // Array3. n ta elementdan tashkil topgan massiv berilgan. Massiv elementlarini quyidagicha chiqaruvchi programma tuzilsin.
@@ -20,8 +37,8 @@
 
 // let arr = [4, 5, 7, 8, 6, 9];
 // let result = "";
-// for (let i = 0; i < arr.length; i++) {
-//     result += `${arr[i]}, ${arr[arr.length - (i+1) ]}, `;
+// for(let i = 0; i < arr.length; i++){
+//     result += ` ${arr[i]}, ${arr[arr.length - (i+1)]},`;
 // }
 // console.log(result);
 
@@ -39,6 +56,10 @@
 //     return result;
 // }
 // console.log(rangeSum(arr, 2, 5));
+
+// Array5. n ta elementdan tashkil topgan massiv berilgan. Massivda qatnashgan sonlardan faqat bittadan chiqaruvchi getSingleArr(arr) tuzilsin.
+// Input: let arr =  [7, 4, 2, 3, 1, 4, 5, 2, 4, 5, 7]
+// Output: [7, 4, 2, 3, 1, 5]
 
 
 // Array5. n ta elementdan tashkil topgan massiv berilgan. Massivda qatnashgan sonlardan faqat bittadan chiqaruvchi getSingleArr(arr) tuzilsin.
@@ -59,13 +80,38 @@
 
 
 // Array6. n ta elementdan iborat massiv berilgan. Massivning eng kichik va eng katta elementlari topilsin va o'rni almashtirilsin.
-
 // Input: [7, 4, 9, 2, 3, 1, 5]
 // Output:
 // Max: 9
 // Min: 1
 // [7, 4, 1, 2, 3, 9, 5]
 
+// let arr = [7, 4, 9, 2, 3, 1, 5];
+// function swapMinMax() {
+//     let max = arr[0];
+//     let min = arr[1];
+//   for(let i = 0; i < arr.length; i++) {
+//     if(arr[i] > max) max = arr[i];
+//     if(arr[i] < min) min = arr[i];
+//   }
+//   arr[arr.indexOf(min)] = max;
+//   arr[arr.indexOf(max)] = min;
+//   return arr;
+// }
+// console.log(swapMinMax())
+
+
+// Array7. n ta elementdan tashkil topgan berilgan. Massiv elementlarini 1 ta o'rin ongga siklik siljituvchi programma tuzilsin. a[0] element qiymati a[1] ga o'tadi, a[1] esa a[2] ga, ..., a[n – 1] qiymati a[0] ga o'tadi.
+// Input: [1, 5, 8, 9, 10]
+// Output: [5, 8, 9, 10, 1]
+
+// let arr = [1, 5, 8, 9, 10];
+// function rotateRightByOne(arr) {
+//     let first = arr.shift();
+//     arr.push(first);
+//     return arr;
+// }
+// console.log(rotateRightByOne(arr));
 
 //                                                  UYGA VAZIFA
 
